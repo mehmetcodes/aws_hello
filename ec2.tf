c27044631e4be75f634256cd1b5cd2e2306cd5c4
Memo Memo <mehmetcodes@gmail.com>
2018-09-05 16:15:51 -0700

    cleaning up

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "example" {
   subnet_id = "${aws_subnet.main_public.id}"
   key_name = "deployer-key"
   user_data = "${file("user-data.example")}"
-  vpc_security_group_ids = ["${aws_security_group.main_allow_ssh.id}"]
+  vpc_security_group_ids = ["${aws_security_group.main_vpc_web.id}","${aws_security_group.main_vpc_ssh.id}"]
   depends_on = ["aws_internet_gateway.gw"]
   tags {
     Name = "main vpc example"
@@ -12,18 +12,21 @@ resource "aws_instance" "example" {
   }
 }
 
-resource "aws_ebs_volume" "example" {
-    availability_zone = "${data.aws_availability_zones.available.names[0]}"
-    size = 1
-    tags {
-        Name = "HelloWorld"
-    }
-}
 
 resource "aws_volume_attachment" "example" {
   device_name = "/dev/sdf"
   volume_id = "${aws_ebs_volume.example.id}"
   instance_id = "${aws_instance.example.id}"
+}
+
+
+resource "aws_ebs_volume" "example" {
+    availability_zone = "${data.aws_availability_zones.available.names[0]}"
+    size = 1
+
+    tags {
+        Name = "HelloWorld Volume"
+    }
 }
 
 resource "aws_instance" "bastion" {
--- security_groups.tf
+++ security_groups.tf
@@ -17,3 +17,43 @@ resource "aws_security_group" "main_allow_ssh" {
       cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_security_group" "main_vpc_ssh" {
+  name        = "allow_ssh_main_vpc"
+  description = "Allow all SSH inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.main_vpc_cidr}"]
+  }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "main_vpc_web" {
+  name        = "allow_vpc_web"
+  description = "Allow all SSH inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+}
